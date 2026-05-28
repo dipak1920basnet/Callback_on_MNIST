e222f5e0a371130c6027955b5f1d8ce5415e8c48
dipak1920basnet <dipak1920basnet@gmail.com>
2026-05-28 19:00:41 +0545

    callback code to stop training the model at 99% accuracy

---
+++ call_back.py
@@ -0,0 +1,8 @@
+import tensorflow as tf 
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs = None):
+        
+        if logs["accuracy"] >= 0.99:
+            print("Model reached 99% accuracy stopping the training")
+            self.model.stop_training = True
