a4c2995f9f9d49d00c9f5511f9f261ae1b585a52
dipak1920basnet <dipak1920basnet@gmail.com>
2026-05-29 00:40:04 +0545

    evaluate the model

--- call_back.py
+++ call_back.py
@@ -4,5 +4,5 @@ class myCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs = None):
         
         if logs["accuracy"] >= 0.99:
-            print("Model reached 99% accuracy stopping the training")
+            print("\nModel reached 99% accuracy stopping the training")
             self.model.stop_training = True
