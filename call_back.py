561357b9c6de65a907636b5cef17b58cf9d56cb4
dipak1920basnet <dipak1920basnet@gmail.com>
2026-05-31 23:56:51 +0545

    add kernel and pooling layer and set callback to 99.5% accuracy

--- call_back.py
+++ call_back.py
@@ -3,6 +3,6 @@ import tensorflow as tf
 class myCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs = None):
         
-        if logs["accuracy"] >= 0.99:
-            print("\nModel reached 99% accuracy stopping the training")
+        if logs["accuracy"] >= 0.995:
+            print("\nReached 99.5% accuracy so cancelling training!")
             self.model.stop_training = True
